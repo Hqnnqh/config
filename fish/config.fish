efbc1cb77c6b9dca1066fdc6cad88b5419367d99
Hqnnqh <hannah@diefluchs.at>
2024-09-30 18:43:50 +0200

    Upload fish config

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	#
+	#fish_add_path ~/.cargo/bin
+	fish_add_path ~/opt/ccross/bin
+	fish_add_path ~/.local/bin
+end
