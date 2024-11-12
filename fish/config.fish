122a5e06280198aa3071894badd7e087b752a4c3
Hqnnqh <hannah@diefluchs.at>
2024-11-12 21:00:25 +0100

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,12 @@ if status is-interactive
 	#fish_add_path ~/.cargo/bin
 	fish_add_path ~/opt/ccross/bin
 	fish_add_path ~/.local/bin
+	fish_add_path ~/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin
+	
+	function man
+		command man $argv | bat -l man
+	end
+
+	alias cat bat
 
 end
